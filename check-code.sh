014b36d979ef07889269c451dadfe23189c4801c
Brian Clapper <bmc@clapper.org>
2023-08-15 15:20:35 -0400

    Added ability to run code through isort, black, and pyright.

---
+++ check-code.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# Run Python checkers and formatters.
+
+echo "Checking types ..."
+pyright || exit 1
+
+echo "Sorting imports in $i"
+isort digest
+
+echo "Formatting $i with black"
+black digest
